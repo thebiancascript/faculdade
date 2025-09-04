619f8c62ec3d2a0d10c9b4ba1eda6b5b31414dde
bs+ <solthings27@gmail.com>
2025-09-03 23:30:09 -0300

    APS2 directory

--- APS2/AgendaDeAniversario.java
+++
@@ -1,9 +0,0 @@
-package br.edu.unipe.aps2.exercicio1;
-
-import java.util.ArrayList;
-
-public interface AgendaDeAniversarios {
-    void adicionarAniversariante(String nome, int dia, int mes);
-    ArrayList<String> obterAniversariantesDoDia(int dia, int mes);
-    void removerAniversariante(String nomeAniversariante);
-}
